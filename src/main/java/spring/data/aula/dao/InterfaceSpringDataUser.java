8806a0310510f7dab887f83bd27e383839a19311
geofrey19 <geofrey19@gmail.com>
2021-07-12 18:53:36 -0300

    anotações do Transational

--- src/main/java/spring/data/aula/dao/InterfaceSpringDataUser.java
+++ src/main/java/spring/data/aula/dao/InterfaceSpringDataUser.java
@@ -1,5 +1,6 @@
 package spring.data.aula.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Modifying;
@@ -15,21 +16,23 @@ import spring.data.aula.model.UsuarioSpringData;
 @Repository
 public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringData, Long> {
 
+	@Transactional(readOnly = true)
 	@Query(value = "Select p from UsuarioSpringData p where p.nome like %?1%")
 	public List<UsuarioSpringData> buscaPorNome(String nome);
 
+	@Transactional(readOnly = true)
 	@Query(value = "Select p from UsuarioSpringData p where p.nome=:paramnome")
 	public UsuarioSpringData buscaPorNomeParam(@Param("paramnome") String paramnome);
 
 	// dize que o método vai fazer modificações no banco de dados
 	@Modifying
 	// para poder excluir e commitar
-	@Transactional
+	@Transactional(rollbackFor = {SQLException.class, NullPointerException.class} )
 	@Query(value = "Delete from UsuarioSpringData u where u.nome= ?1")
 	public void deletePorNome(String nome);
 	
 	@Modifying
-	@Transactional
+	@Transactional(rollbackFor = {SQLException.class, NullPointerException.class} )
 	@Query(value = "Update UsuarioSpringData u set u.email = ?1 where u.nome = ?2 ")
 	public void updatePorNome(String email, String nome);
 }
